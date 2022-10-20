e7d9466f19a387afa66ba688e068e03f51729d5d
Devon Morrison <115171650+Phoenix5545@users.noreply.github.com>
2022-10-20 16:03:42 +0000

    Initial commit

---
+++ game2/script.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("grassBackground");
+var ctx = canvas.getContext("2d");
+var spriteMcIdleRight = "/game2/characters/mc/idleRight.png";
+var spriteMcRunningRight = "/game2/characters/mc/runningRight.png";
+var spriteMcShootingRight = "/game2/characters/mc/shootingRight.png";
+var spriteMcShotRight = "/game2/characters/mc/shotRight.png";
